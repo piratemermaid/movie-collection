c86d04df9158404d409cd27caa397cff1e0766b8
Ariel-Schnur-Bose <ariel_schnur@bose.com>
2018-09-28 13:36:07 -0400

    Incorporate add to wishlist

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import TableList from "./TableList";
 
 const Wishlist = props => {
   return (
     <div>
+      <Link to="/add/wishlist">
+        <i className="material-icons small icon-link float-right">add_circle</i>
+      </Link>
       <TableList movies={props.wishlist} />
     </div>
   );
