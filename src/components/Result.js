ce7fb22643aa6f52a93756a584906d9da20e2add
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-09-28 14:10:51 -0400

    Add view unwatched

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,14 +5,28 @@ import { Link } from "react-router-dom";
 import TableList from "./TableList";
 
 const Result = props => {
-  const tag = props.match.params.tag;
-  const matches = props.movies.filter(movie => {
-    if (movie.tags.includes(tag)) {
-      return movie.title;
-    } else {
-      return null;
+  let title, matches, tag;
+  console.log(props);
+  if (props.match.params.tag) {
+    title = tag;
+    tag = props.match.params.tag;
+    matches = props.movies.filter(movie => {
+      if (movie.tags.includes(tag)) {
+        return movie.title;
+      } else {
+        return null;
+      }
+    });
+  } else {
+    title = String(props.match.path.substring(props.match.path.length, 8));
+    if (title === "unwatched") {
+      matches = props.movies.filter(movie => {
+        if (!movie.watched) {
+          return movie.title;
+        } else return null;
+      });
     }
-  });
+  }
 
   return (
     <div>
@@ -20,7 +34,7 @@ const Result = props => {
         <i className="material-icons small icon-link">arrow_back</i>
       </Link>
       <br />
-      <h5>Movies matching "{tag}"</h5>
+      <h5>Movies matching "{title}"</h5>
       <TableList movies={matches} tag={tag} />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -10,7 +10,7 @@ const Search = props => {
         <Link
           className="search-tag col s2"
           key={i}
-          to={`/search/${tagList[i]}`}
+          to={`/search/tags/${tagList[i]}`}
         >
           {tagList[i]}
         </Link>
@@ -20,7 +20,26 @@ const Search = props => {
     return tagsRender;
   }
 
-  return <div className="row">{renderAllTags()}</div>;
+  return (
+    <div className="row">
+      <div className="search-section">
+        <h5>view all unwatched</h5>
+        <Link to="/search/unwatched">
+          <i className="material-icons small icon-link">arrow_forward</i>
+        </Link>
+      </div>
+      <hr />
+      <div className="search-section">
+        <h5>search by year</h5>
+      </div>
+      <hr />
+      <div className="search-section">
+        <h5>search by tag(s)</h5>
+        <p>Click 1 or more tags you want to search for and hit "Search"</p>
+        {renderAllTags()}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
