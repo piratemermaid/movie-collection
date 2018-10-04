76e1e3313e18780d34965ada3ac36f578f2d2701
Ariel-Schnur-Bose <ariel_schnur@bose.com>
2018-10-04 11:34:07 -0400

    Add UI for sorting TableList

--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const TableList = props => {
-  function removeFromList(title) {
+/**
+ * @param {string} sortBy: 'dateAdded', 'year', 'title'
+ * @param {boolean} ascending: true or false
+ * e.g. sortBy: 'title', ascending: true = alphabetical order,
+ *      sortBy: 'title', ascending: false = reverse alphabetical order
+ */
+class TableList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { sortBy: "dateAdded", ascending: true };
+  }
+
+  removeFromList(title) {
     const rows = document.querySelectorAll(".table-list-row");
     let val;
     for (let i in rows) {
@@ -15,7 +26,7 @@ const TableList = props => {
     }
   }
 
-  function renderMovies(movies) {
+  renderMovies(movies) {
     let rows = [];
 
     for (let i in movies) {
@@ -28,36 +39,39 @@ const TableList = props => {
         >
           <td>
             {movie.title}
-            {props.deletable ? (
+            {this.props.deletable ? (
               <i
                 className="material-icons tiny icon-link"
-                onClick={() => removeFromList(movie.title)}
+                onClick={() => this.removeFromList(movie.title)}
               >
                 delete
               </i>
             ) : null}
-            {props.editable ? (
+            {this.props.editable ? (
               <Link to={`/edit/${movie.title}`}>
                 <i className="material-icons tiny icon-link">edit</i>
               </Link>
             ) : null}
           </td>
-          <td>{renderTags(movie.tags)}</td>
+          <td>{this.renderTags(movie.tags)}</td>
           <td>{movie.year}</td>
           <td>{movie.watched ? "Yes" : "No"}</td>
         </tr>
       );
     }
 
-    return rows;
+    // TODO: use state to sort rows
+    let sortedRows = rows;
+
+    return sortedRows;
   }
 
-  function renderTags(tags) {
+  renderTags(tags) {
     let renderTagArr = [];
     let tagClass;
     let tagsArr = [];
-    if (props.tags) {
-      tagsArr = props.tags.split("&");
+    if (this.props.tags) {
+      tagsArr = this.props.tags.split("&");
     }
 
     for (let i in tags) {
@@ -74,19 +88,89 @@ const TableList = props => {
     return renderTagArr;
   }
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Tags</th>
-          <th>Year</th>
-          <th>Watched?</th>
-        </tr>
-      </thead>
-      <tbody>{renderMovies(props.movies)}</tbody>
-    </table>
-  );
-};
+  changeSortMethod(type) {
+    if (this.state.sortBy === type) {
+      if (this.state.ascending) {
+        this.setState({ ascending: false });
+      } else {
+        this.setState({ ascending: true });
+      }
+    } else {
+      this.setState({ sortBy: type, ascending: true });
+    }
+  }
+
+  render() {
+    const { sortBy, ascending } = this.state;
+    return (
+      <div>
+        <div id="sort-options">
+          Sort options:{" "}
+          <a
+            onClick={() => this.changeSortMethod("dateAdded")}
+            className={
+              sortBy === "dateAdded"
+                ? "sort-option sort-option-active"
+                : "sort-option"
+            }
+          >
+            date added
+          </a>
+          {sortBy === "dateAdded" && ascending === true ? (
+            <i className="material-icons tiny icon-link">arrow_drop_up</i>
+          ) : null}
+          {sortBy === "dateAdded" && ascending === false ? (
+            <i className="material-icons tiny icon-link">arrow_drop_down</i>
+          ) : null}
+          {" | "}
+          <a
+            onClick={() => this.changeSortMethod("title")}
+            className={
+              sortBy === "title"
+                ? "sort-option sort-option-active"
+                : "sort-option"
+            }
+          >
+            title
+          </a>
+          {sortBy === "title" && ascending === true ? (
+            <i className="material-icons tiny icon-link">arrow_drop_up</i>
+          ) : null}
+          {sortBy === "title" && ascending === false ? (
+            <i className="material-icons tiny icon-link">arrow_drop_down</i>
+          ) : null}
+          {" | "}
+          <a
+            onClick={() => this.changeSortMethod("year")}
+            className={
+              sortBy === "year"
+                ? "sort-option sort-option-active"
+                : "sort-option"
+            }
+          >
+            year
+          </a>
+          {sortBy === "year" && ascending === true ? (
+            <i className="material-icons tiny icon-link">arrow_drop_up</i>
+          ) : null}
+          {sortBy === "year" && ascending === false ? (
+            <i className="material-icons tiny icon-link">arrow_drop_down</i>
+          ) : null}
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Tags</th>
+              <th>Year</th>
+              <th>Watched?</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderMovies(this.props.movies)}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default TableList;
