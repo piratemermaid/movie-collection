e6a146476a2a2f63457c4afaa741da2ded2d573a
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-10-08 15:03:57 -0400

    Update personal wishlist

--- src/myMovies.js
+++ src/myMovies.js
@@ -1,6 +1,63 @@
 export function fillMyMovies() {
   const collection = [];
-  const wishlist = [];
+  const wishlist = [
+    {
+      added: "10/08/18",
+      tags: ["comedy", "#deep"],
+      title: "The Truman Show",
+      watched: true,
+      year: "?",
+      releaseDate: "released"
+    },
+    {
+      added: "10/01/18",
+      tags: ["romantic", "comedy"],
+      title: "Crazy Rich Asians",
+      watched: false,
+      year: "2018",
+      releaseDate: "?"
+    },
+    {
+      added: "10/07/18",
+      tags: ["disney", "animated", "superhero"],
+      title: "Incredibles 2",
+      watched: true,
+      year: "2018",
+      releaseDate: "October 23"
+    },
+    {
+      added: "10/01/18",
+      tags: ["comedy", "history"],
+      title: "KKKlansman",
+      watched: false,
+      year: "2018",
+      releaseDate: "?"
+    },
+    {
+      added: "10/01/18",
+      tags: ["disney", "musical", "nostalgia"],
+      title: "Mary Poppins Returns",
+      watched: false,
+      year: "2018",
+      releaseDate: "theaters soon?"
+    },
+    {
+      added: "10/01/18",
+      tags: ["disney", "animated"],
+      title: "Wreck-It Ralph 2",
+      watched: false,
+      year: "2018",
+      releaseDate: "theaters soon?"
+    },
+    {
+      added: "10/01/18",
+      tags: ["nerdy", "magic"],
+      title: "Fantastic Beasts: The Crimes of Grindelwald",
+      watched: false,
+      year: "2018",
+      releaseDate: "theaters in Nov"
+    }
+  ];
 
   return { collection, wishlist };
 }
