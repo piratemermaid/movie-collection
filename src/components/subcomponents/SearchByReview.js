f424f8d6ef0490d1a35514ef102705ece456ca6e
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-10-09 14:33:01 -0400

    Finish SearchByReview

--- src/components/subcomponents/SearchByReview.js
+++ src/components/subcomponents/SearchByReview.js
@@ -4,25 +4,25 @@ import { withRouter } from "react-router";
 /**
  * Options:
  * exact: searching for exact number of stars.
- * andUp: *number* and up stars aka good movies
- * andDown: *number* and down aka bad movies
+ * up: *number* and up stars aka good movies
+ * down: *number* and down aka bad movies
  */
 class SearchByReview extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { option: "exact", stars: false, formErr: "" };
+    this.state = { option: "exact", review: false, formErr: "" };
   }
 
   searchByReview(e) {
     e.preventDefault();
     if (!this.state.option) {
       this.setState({ formErr: "Please choose a search option" });
+    } else {
+      //   '/search/reviews/:review/:option'
+      let url = `/search/reviews/${this.state.review}/${this.state.option}`;
+      this.props.history.push(url);
     }
-    if (!this.state.stars) {
-      this.setState({ formErr: "Please choose a star amount" });
-    }
-    console.log(this.state.option, this.state.stars);
   }
 
   onOptionChange(option) {
@@ -34,10 +34,58 @@ class SearchByReview extends Component {
     }
   }
 
+  onReviewChange(num) {
+    if (num === this.state.review) {
+      this.setState({ review: false });
+    } else {
+      this.setState({ review: num });
+    }
+  }
+
+  getStarClass(num) {
+    let starClass = "material-icons small icon-link review-star";
+    if (this.state.review && this.state.review >= num) {
+      starClass += " review-star-active";
+    }
+    return starClass;
+  }
+
   render() {
     return (
       <form onSubmit={e => this.searchByReview(e)}>
-        <div className="input-field col s12">row of stars</div>
+        <div className="input-field col s12">
+          <p className="form-label">Your review</p>
+          <i
+            className={this.getStarClass(1)}
+            onClick={() => this.onReviewChange(1)}
+          >
+            star
+          </i>
+          <i
+            className={this.getStarClass(2)}
+            onClick={() => this.onReviewChange(2)}
+          >
+            star
+          </i>
+          <i
+            className={this.getStarClass(3)}
+            onClick={() => this.onReviewChange(3)}
+          >
+            star
+          </i>
+          <i
+            className={this.getStarClass(4)}
+            onClick={() => this.onReviewChange(4)}
+          >
+            star
+          </i>
+          <i
+            className={this.getStarClass(5)}
+            onClick={() => this.onReviewChange(5)}
+          >
+            star
+          </i>
+        </div>
         <div className="row col s12">
           <p className="info">Options:</p>
         </div>
@@ -53,23 +101,23 @@ class SearchByReview extends Component {
         </div>
         <div className="input-field col s12" style={{ marginBottom: "20px" }}>
           <input
-            id="review-andUp"
+            id="review-up"
             type="checkbox"
-            checked={this.state.option === "andUp"}
-            onChange={() => this.onOptionChange("andUp")}
+            checked={this.state.option === "up"}
+            onChange={() => this.onOptionChange("up")}
             className="checkbox-blue"
           />
-          <label htmlFor="review-andUp">Chosen star number and up</label>
+          <label htmlFor="review-up">Chosen star number and up</label>
         </div>
         <div className="input-field col s12" style={{ marginBottom: "20px" }}>
           <input
-            id="review-andDown"
+            id="review-down"
             type="checkbox"
-            checked={this.state.option === "andDown"}
-            onChange={() => this.onOptionChange("andDown")}
+            checked={this.state.option === "down"}
+            onChange={() => this.onOptionChange("down")}
             className="checkbox-blue"
           />
-          <label htmlFor="review-andDown">Chosen star number and down</label>
+          <label htmlFor="review-down">Chosen star number and down</label>
         </div>
         <br />
         <div className="row col s12" style={{ marginTop: "20px" }}>
@@ -89,4 +137,4 @@ class SearchByReview extends Component {
   }
 }
 
-export default SearchByReview;
+export default withRouter(SearchByReview);
