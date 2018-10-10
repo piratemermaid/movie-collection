901c5ce0aeb7b51155e40f9e24cd069dcb8c8620
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2018-10-10 13:53:49 -0400

    Make Search a dumb component

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,73 +10,67 @@ import SearchByReview from "./subcomponents/SearchByReview";
  * e.g. when user clicks "search by year", display a form
  * where they can enter the year and search options
  */
-class Search extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Search = props => {
+  let cardClass = "card search-section link-hover card-link";
 
-  render() {
-    let cardClass = "card search-section link-hover card-link";
-
-    return (
-      <div className="row flex">
-        <div className="col s3 flex center-align">
-          <div
-            className={
-              this.props.display === "tags"
-                ? `${cardClass} card-link-active`
-                : cardClass
-            }
-            onClick={() => this.props.changeSearchDisplay("tags")}
-          >
-            <h6>search by tag(s)</h6>
-          </div>
-        </div>
-        <div className="col s3 flex center-align">
-          <div
-            className={
-              this.props.display === "year"
-                ? `${cardClass} card-link-active`
-                : cardClass
-            }
-            onClick={() => this.props.changeSearchDisplay("year")}
-          >
-            <h6>search by year</h6>
-          </div>
-        </div>
-        <div className="col s3 flex center-align">
-          <div
-            className={cardClass}
-            onClick={() => this.props.history.push("/search/unwatched")}
-          >
-            <h6>view all unwatched</h6>
-            <i className="material-icons small icon-link">arrow_forward</i>
-          </div>
-        </div>
-        <div className="col s3 flex center-align">
-          <div
-            className={
-              this.props.display === "review"
-                ? `${cardClass} card-link-active`
-                : cardClass
-            }
-            onClick={() => this.props.changeSearchDisplay("review")}
-          >
-            <h6>search by review</h6>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s12">
-            {this.props.display === "year" ? <SearchByYear /> : null}
-            {this.props.display === "tags" ? (
-              <SearchByTag getAllTags={this.props.getAllTags} />
-            ) : null}
-            {this.props.display === "review" ? <SearchByReview /> : null}
-          </div>
+  return (
+    <div className="row flex">
+      <div className="col s3 flex center-align">
+        <div
+          className={
+            props.display === "tags"
+              ? `${cardClass} card-link-active`
+              : cardClass
+          }
+          onClick={() => props.changeSearchDisplay("tags")}
+        >
+          <h6>search by tag(s)</h6>
         </div>
       </div>
-    );
-  }
-}
+      <div className="col s3 flex center-align">
+        <div
+          className={
+            props.display === "year"
+              ? `${cardClass} card-link-active`
+              : cardClass
+          }
+          onClick={() => props.changeSearchDisplay("year")}
+        >
+          <h6>search by year</h6>
+        </div>
+      </div>
+      <div className="col s3 flex center-align">
+        <div
+          className={cardClass}
+          onClick={() => props.history.push("/search/unwatched")}
+        >
+          <h6>view all unwatched</h6>
+          <i className="material-icons small icon-link">arrow_forward</i>
+        </div>
+      </div>
+      <div className="col s3 flex center-align">
+        <div
+          className={
+            props.display === "review"
+              ? `${cardClass} card-link-active`
+              : cardClass
+          }
+          onClick={() => props.changeSearchDisplay("review")}
+        >
+          <h6>search by review</h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          {props.display === "year" ? <SearchByYear /> : null}
+          {props.display === "tags" ? (
+            <SearchByTag getAllTags={props.getAllTags} />
+          ) : null}
+          {props.display === "review" ? <SearchByReview /> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(Search);
