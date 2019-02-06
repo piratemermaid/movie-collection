f7fb9b326137c87639a98f011d06ec5d6a3b79b5
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2019-02-06 10:52:07 -0500

    Make Header stateless

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,15 +2,9 @@ import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { active: "search" };
-    }
-
-    getClassName(link) {
-        const path = this.props.location.pathname;
+const Header = props => {
+    function getClassName(link) {
+        const path = props.location.pathname;
         if (path === link) {
             return "active";
         } else {
@@ -20,31 +14,29 @@ class Header extends Component {
         }
     }
 
-    render() {
-        return (
-            <nav>
-                <div className="nav-wrapper blue lighten-2">
-                    <ul id="nav-mobile">
-                        <li className={this.getClassName("/")}>
-                            <Link to="/">Pick a Movie</Link>
-                        </li>
-                        <li className={this.getClassName("/collection")}>
-                            <Link to="/collection">My Collection</Link>
-                        </li>
-                        <li className={this.getClassName("/wishlist")}>
-                            <Link to="/wishlist">Wishlist</Link>
-                        </li>
-                        <li className={this.getClassName("/imdb")}>
-                            <Link to="/imdb">IMDB Search</Link>
-                        </li>
-                        <li className={this.getClassName("/account")}>
-                            <Link to="/account">Account</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
-}
+    return (
+        <nav>
+            <div className="nav-wrapper blue lighten-2">
+                <ul id="nav-mobile">
+                    <li className={getClassName("/")}>
+                        <Link to="/">Pick a Movie</Link>
+                    </li>
+                    <li className={getClassName("/collection")}>
+                        <Link to="/collection">My Collection</Link>
+                    </li>
+                    <li className={getClassName("/wishlist")}>
+                        <Link to="/wishlist">Wishlist</Link>
+                    </li>
+                    <li className={getClassName("/imdb")}>
+                        <Link to="/imdb">IMDB Search</Link>
+                    </li>
+                    <li className={getClassName("/account")}>
+                        <Link to="/account">Account</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
 
 export default withRouter(Header);
