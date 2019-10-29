9cce90f372bce7c2600d1392e7156223a2f17664
Ariel-Schnur-Bose <schnur.ariel@gmail.com>
2019-10-29 15:27:57 -0400

    start server setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParse = require("body-parser");
+const accountRouter = require("./routes/account");
+
+const app = express();
+
+app.use("/account", accountRouter);
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log("app listening on port", port);
+
+module.exports = app;
