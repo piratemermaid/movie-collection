57dc1b39563172ed53fb2699f68204a635cd7ff3
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-07 12:28:33 -0400

    commit build

---
+++ client/build/precache-manifest.1b614c47ed21ff54e71a78385ea0df24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9c3b0b8fdcf68d337c27e1a471057f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62b46ac07927b419b1b",
+    "url": "/static/css/main.a2ce711e.chunk.css"
+  },
+  {
+    "revision": "a47843c59bf606da6580",
+    "url": "/static/js/2.be425689.chunk.js"
+  },
+  {
+    "revision": "97615692a240ada5e5f775b942e9e981",
+    "url": "/static/js/2.be425689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62b46ac07927b419b1b",
+    "url": "/static/js/main.9689ff17.chunk.js"
+  },
+  {
+    "revision": "0d4d74794519bce1b988",
+    "url": "/static/js/runtime-main.afd20e7d.js"
+  }
+]);
